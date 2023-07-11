402b8dc198957205baf716665550308f3451f7c3
Gustavo <gustavofr0411@gmail.com>
2023-07-10 21:34:49 -0300

    Método mostrar() dividido em duas partes

--- src/App.java
+++ src/App.java
@@ -17,37 +17,40 @@ public class App {
                 + ". Em um certo dia, você é conjurado pelo Rei Salaham"
                                 + " para recuperar o Anel do Poder roubado por seu rival, o " + magoDoMal.nome()
                                 + ". Se não aceitar, pode pôr a vida do " + rei.nome()
-                                + " em perigo. Aceitar a proposta?", "ACEITAR - DIGITE \"S\"", "RECUSAR - DIGITE \"N\"",
-                                null, protagonista, 100);
+                                + " em perigo. Aceitar a proposta?", null, "ACEITAR - DIGITE \"S\"", "RECUSAR - DIGITE \"N\"", protagonista, 100);
 
-                capitulo1.mostrar(continuar);
+                capitulo1.mostrar1(continuar);
+                capitulo1.mostrar2(continuar);
                 int escolhaCap1 = capitulo1.escolher(continuar, "n", "s");
 
                 if (escolhaCap1 == 1) {
                         Capitulo capitulo1_1 = new Capitulo(null, "Ao recusar, o "
                                         + magoDoMal.nome()
-                                        + " permanece vivo e executa o rei com todo o poder do Anel. No dia seguinte, o Mago do Mal"
+                                        + " permanece vivo e executa o rei com todo o poder do Anel.", "No dia seguinte, o Mago do Mal"
                                         + " vem até você e o oferece uma"
                                         + " proposta de servir a ele. Se não servir, terá que combatê-lo.",
                                         "COMBATER - DIGITE \"LUTAR\"",
-                                        "SERVIR - DIGITE \"SERVIR\"", rei.atacado(rei, rei.vida()), rei, rei.vida());
-                        capitulo1_1.mostrar(continuar);
+                                        "SERVIR - DIGITE \"SERVIR\"", protagonista, protagonista.vida());
+                        capitulo1_1.mostrar1(continuar);
                         rei.atacado(rei, 100);
+                        capitulo1_1.mostrar2(continuar);
                         int escolhaCap1_1 = capitulo1_1.escolher(continuar, "servir", "lutar");
                         if (escolhaCap1_1 == 1) {
                                 Capitulo capitulo1_2 = new Capitulo(null, "Você decide servir o " + magoDoMal.nome()
                                                 + ". Com o tempo, você se torna corrompido e deseja matar e conquistar cada território,"
                                                 + " tornando-se um forte aliado.", null, null, null, protagonista,
                                                 protagonista.vida());
-                                capitulo1_2.mostrar(continuar);
+                                capitulo1_2.mostrar1(continuar);
                                 System.out.print("[FINAL 3 - CORROMPIDO PELO MAL]");
                         } else if (escolhaCap1_1 == 2) {
                                 Capitulo capitulo1_2 = new Capitulo(null, "Você decide lutar contra o "
                                                 + magoDoMal.nome()
-                                                + ". Infelizmente, ele já estava forte demais para ser combatido. Você então é derrotado.",
-                                                null, null, protagonista.atacado(protagonista, protagonista.vida()),
+                                                + ". Infelizmente, ele já estava forte demais para ser combatido. Você então é derrotado.", null,
+                                                null, null,
                                                 protagonista, protagonista.vida());
-                                capitulo1_2.mostrar(continuar);
+                                capitulo1_2.mostrar1(continuar);
+                                protagonista.atacado(protagonista, protagonista.vida());
+                                capitulo1_2.mostrar2(continuar);
                                 System.out.print("[FINAL 4 - DERROTADO COM DIGNIDADE]");
                         }
                 } else if (escolhaCap1 == 2) {
@@ -56,29 +59,32 @@ public class App {
                         Capitulo capitulo2 = new Capitulo("[CAPÍTULO 2 - A JORNADA]",
                                         "Você aceita o convite do rei e parte para recuperar o"
                                                         + " Anel do Poder. Há dois caminhos para seguir: O da direita é mais seguro, porém é mais longo. O da esquerda é"
-                                                        + " o mais rápido, porém com muito inimigos. Qual caminnho seguir?",
+                                                        + " o mais rápido, porém com muito inimigos. Qual caminnho seguir?", null,
                                         "IR PELA DIREITA - DIGITE \"DIREITA\"",
-                                        "IR PELA ESQUERDA - DIGITE \"ESQUERDA\"", null, protagonista,
+                                        "IR PELA ESQUERDA - DIGITE \"ESQUERDA\"", protagonista,
                                         protagonista.vida());
-                        capitulo2.mostrar(continuar);
+                        capitulo2.mostrar1(continuar);
+                        capitulo2.mostrar2(continuar);
                         int escolhaCapitulo2 = capitulo2.escolher(continuar, "esquerda", "direita");
                         if (escolhaCapitulo2 == 1) {
                                 Capitulo capitulo2_1 = new Capitulo("[CAPÍTULO 2.1 - SEGUINDO PELA ESQUERDA]",
                                                 "Você decide seguir pelo caminho"
                                                                 + " da esquerda. A viagem é curta, porém cheia de inimigos para derrotar,"
-                                                                + " o que lhe custou ferimentos intensos.",
-                                                null, null, protagonista.atacado(protagonista, 40),
-                                                protagonista, protagonista.vida());
-                                capitulo2_1.mostrar(continuar);
+                                                                + " o que lhe custou ferimentos intensos.", null,
+                                                null, null, protagonista, protagonista.vida());
+                                capitulo2_1.mostrar1(continuar);
+                                protagonista.atacado(protagonista, 40);
+                                capitulo2_1.mostrar2(continuar);
                                 caminhoSeguido = 1;
                         } else if (escolhaCapitulo2 == 2) {
                                 Capitulo capitulo2_2 = new Capitulo("[CAPÍTULO 2.2 - SEGUINDO PELA DIREITA]",
                                                 "Você decide seguir pelo caminho da direita."
                                                                 + " A viagem é longa, com alguns poucos inimigos para derrotar, o que lhe custou ferimentos leves.",
-                                                null, null,
-                                                protagonista.atacado(protagonista, 10), protagonista,
+                                                null, null, null, protagonista,
                                                 protagonista.vida());
-                                capitulo2_2.mostrar(continuar);
+                                capitulo2_2.mostrar1(continuar);
+                                protagonista.atacado(protagonista, 10);
+                                capitulo2_2.mostrar2(continuar);
                                 caminhoSeguido = 2;
                         }
 
@@ -86,29 +92,32 @@ public class App {
 
                         Capitulo capitulo3 = new Capitulo("[CAPÍTULO 3 - A CHEGADA À TORRE]",
                                         "Você chega até a torre. O Mago do Mal"
-                                                        + " deve estar no topo da torre, porém você deve derrotar alguns inimigos que protegiam a torre.",
-                                        null, null,
-                                        protagonista.atacado(protagonista, 30), protagonista, protagonista.vida());
-                        capitulo3.mostrar(continuar);
+                                                        + " deve estar no topo da torre, porém você deve derrotar alguns inimigos que protegiam a torre.", null,
+                                        null, null, protagonista, protagonista.vida());
+                        capitulo3.mostrar1(continuar);
+                        protagonista.atacado(protagonista, 30);
+                        capitulo3.mostrar2(continuar);
                         if (caminhoSeguido == 1) {
                                 Capitulo capitulo3_1 = new Capitulo(null,
                                                 "Você chega até ao topo da torre e enfrenta o Mago do Mal"
                                                                 + " em uma batalha. Porém, os ferimentos dos inimigos do caminho afetaram sua vitalidade e o deixou fraco para"
-                                                                + " a batalha, o que levou à sua derrota.",
-                                                null, null,
-                                                protagonista.atacado(protagonista, protagonista.vida()), protagonista,
+                                                                + " a batalha, o que levou à sua derrota.", null,
+                                                null, null, protagonista,
                                                 protagonista.vida());
-                                capitulo3_1.mostrar(continuar);
+                                capitulo3_1.mostrar1(continuar);
+                                protagonista.atacado(protagonista, protagonista.vida());
+                                capitulo3_1.mostrar2(continuar);
                                 System.out.println("[FINAL 2 - GUERREIRO DESTEMIDO]");
                         } else if (caminhoSeguido == 2) {
                                 Capitulo capitulo3_2 = new Capitulo(null,
                                                 "Você chega até o topo da torre e enfrenta o Mago do Mal"
                                                                 + " em uma batalha. Com vitalidade o suficiente, você consegue derrotar o Mago do Mal e devolver o Anel do Poder"
-                                                                + " de volta para o Rei de forma segura.",
-                                                null, null,
-                                                protagonista.atacado(protagonista, 50), protagonista,
+                                                                + " de volta para o Rei de forma segura.", null,
+                                                null, null, protagonista,
                                                 protagonista.vida());
-                                capitulo3_2.mostrar(continuar);
+                                capitulo3_2.mostrar1(continuar);
+                                protagonista.atacado(protagonista, protagonista.vida());
+                                capitulo3_2.mostrar2(continuar);
                                 System.out.println("[FINAL 1 - O ANEL É RECUPERADO]");
                         }
                 }
--- src/Capitulo.java
+++ src/Capitulo.java
@@ -3,42 +3,43 @@ import java.util.ArrayList;
 
 public class Capitulo {
     String nome;
-    String texto;
+    String texto1;
+    String texto2;
     String escolha1;
     String escolha2;
-    String consequencia;
     Personagem personagem;
     int vida;
     ArrayList<String> arrayEscolhas;
 
-    public Capitulo(String nome, String texto, String escolha1, String escolha2,
-            String consequenciaDecisao, Personagem personagem, int vida) {
+    public Capitulo(String nome, String texto1, String texto2, String escolha1, String escolha2 , Personagem personagem, int vida) {
         this.nome = nome;
-        this.texto = texto;
+        this.texto1 = texto1;
+        this.texto2 = texto2;
         this.arrayEscolhas = new ArrayList<String>();
         this.arrayEscolhas.add(escolha1);
         this.arrayEscolhas.add(escolha2);
-        this.consequencia = consequenciaDecisao;
         this.personagem = personagem;
         this.vida = vida;
     }
 
-    public void mostrar(Scanner continuar) {
+    public void mostrar1(Scanner continuar) {
         if (this.nome != null) {
             System.out.println(this.nome);
             continuar.nextLine();
         }
-        System.out.println(this.texto);
+        System.out.println(this.texto1);
         continuar.nextLine();
+    }
+
+    public void mostrar2(Scanner continuar) {
+        if (this.texto2 != null) {
+            System.out.println(texto2);
+        }
         for (int escolhas = 0; escolhas < arrayEscolhas.size(); escolhas++) {
             if (arrayEscolhas.get(escolhas) != null) {
                 System.out.println(arrayEscolhas.get(escolhas));
             }
         }
-        if (this.consequencia != null) {
-            System.out.println(this.consequencia);
-        }
-
     }
 
     public int escolher(Scanner continuar, String escolha1, String escolha2) {
