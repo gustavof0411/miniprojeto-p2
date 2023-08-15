88b8272f0a2d92f22b1137c6b0760eb7a2e7462e
gustavof0411 <136523482+gustavof0411@users.noreply.github.com>
2023-08-15 19:41:50 -0300

    Testes MP7 seguindo a resolução do MP7

--- src/ObterDado.java
+++ src/ObterDado.java
@@ -1,8 +1,6 @@
 import java.util.HashMap;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.util.Scanner;
+import java.io.File;
 
 public class ObterDado {
 
@@ -10,22 +8,21 @@ public class ObterDado {
         HashMap<String, Personagem> personagens = new HashMap<String, Personagem>();
         File arquivo = new File(diretorio);
         try {
-            FileReader fileReader = new FileReader(arquivo);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            Scanner scanner = new Scanner(arquivo, "UTF-8");
             String algumaLinha = "";
 
-            while ((algumaLinha = bufferedReader.readLine()) != null) {
+            while (scanner.hasNextLine()) {
                 while (!algumaLinha.equals("PERSONAGEM")) {
-                    algumaLinha = bufferedReader.readLine();
+                    algumaLinha = scanner.nextLine();
                 }
                 Personagem algumPersonagem = new Personagem("", 0);
-                algumaLinha = bufferedReader.readLine();
-                algumPersonagem.setNome(bufferedReader.readLine());
-                algumaLinha = bufferedReader.readLine();
-                algumPersonagem.setVida(Integer.parseInt(bufferedReader.readLine()));
+                algumaLinha = scanner.nextLine();
+                algumPersonagem.setNome(scanner.nextLine());
+                algumaLinha = scanner.nextLine();
+                algumPersonagem.setVida(Integer.parseInt(scanner.nextLine()));
                 personagens.put(algumPersonagem.getNome(), algumPersonagem);
-                bufferedReader.close();
             }
+            scanner.close();
         } catch (Exception e) {
             System.out.println("Erro ao carregar algum personagem.");
         }
@@ -36,56 +33,60 @@ public class ObterDado {
             String diretório, HashMap<String, Personagem> personagens,
             Scanner escaneadorDeEscolhas) {
         HashMap<String, Capitulo> capitulos = new HashMap<String, Capitulo>();
-        File arquivo = new File("HistoriaMiniprojeto/rsc/capitulos.txt");
+        File arquivo = new File(diretório);
         try {
-            FileReader fileReader = new FileReader(arquivo);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            Scanner scanner = new Scanner(arquivo, "UTF-8");
             String algumaLinha = "";
 
-            while ((algumaLinha = bufferedReader.readLine()) != null) {
-                while (!algumaLinha.equals("CAPÍTULO")) {
-                    algumaLinha = bufferedReader.readLine();
+            while ((algumaLinha = scanner.nextLine()) != null) {
+                while (!algumaLinha.equalsIgnoreCase("CAPITULO") ||
+                        !algumaLinha.equalsIgnoreCase("ESCOLHA")) {
+                    algumaLinha = scanner.nextLine();
                 }
-                Capitulo algumCapitulo = new Capitulo("", "", new Personagem("", 0), 0, "", "");
-                algumaLinha = bufferedReader.readLine(); // NOME
-                algumaLinha = bufferedReader.readLine();
-                if (!(algumaLinha).equalsIgnoreCase("null")) {
-                    algumCapitulo.setNome(algumaLinha);
-                    algumaLinha = bufferedReader.readLine(); // TEXTO
-                } else {
-                    algumCapitulo.setNome(null);
-                    algumaLinha = bufferedReader.readLine(); // TEXTO
+                if (algumaLinha.equalsIgnoreCase("CAPÍTULO")) {
+                    Capitulo algumCapitulo = new Capitulo("", "", new Personagem("", 0), 0, "", "");
+                    algumaLinha = scanner.nextLine(); // NOME
+                    algumaLinha = scanner.nextLine();
+                    if (!(algumaLinha).equalsIgnoreCase("null")) {
+                        algumCapitulo.setNome(algumaLinha);
+                        algumaLinha = scanner.nextLine(); // TEXTO
+                    } else {
+                        algumCapitulo.setNome(null);
+                        algumaLinha = scanner.nextLine(); // TEXTO
+                    }
+                    algumaLinha = scanner.nextLine();
+                    algumCapitulo.setTexto(algumaLinha);
+                    algumaLinha = scanner.nextLine(); // PERSONAGEM
+                    algumaLinha = scanner.nextLine();
+                    algumCapitulo.setPersonagem(personagens.get(algumaLinha));
+                    algumaLinha = scanner.nextLine(); // VIDA
+                    algumaLinha = scanner.nextLine();
+                    algumCapitulo.setVida(personagens.get(algumaLinha).getVida());
+                    algumaLinha = scanner.nextLine(); // CONSEQUÊNCIA
+                    algumaLinha = scanner.nextLine();
+                    if (!algumaLinha.equalsIgnoreCase("null")) {
+                        String vidaConsequencia = algumaLinha;
+                        vidaConsequencia = scanner.nextLine();
+                        algumCapitulo.setConsequencia(personagens.get(algumaLinha)
+                                .getMensagemAtk((personagens.get(algumaLinha)), Integer.parseInt(vidaConsequencia)));
+                        algumaLinha = scanner.nextLine();
+                    } else {
+                        algumCapitulo.setConsequencia(null);
+                        algumaLinha = scanner.nextLine();
+                        algumaLinha = scanner.nextLine();
+                    }
+                    algumaLinha = scanner.nextLine(); // FINAL DO CAPÍTULO
+                    if (!algumaLinha.equalsIgnoreCase("null")) {
+                        algumCapitulo.setFinalCap(algumaLinha);
+                    } else {
+                        algumCapitulo.setFinalCap(null);
+                    }
+                    capitulos.put(algumCapitulo.getNome(), algumCapitulo);
+                } else if (algumaLinha.equalsIgnoreCase("ESCOLHA")) {
+
                 }
-                algumaLinha = bufferedReader.readLine();
-                algumCapitulo.setTexto(algumaLinha);
-                algumaLinha = bufferedReader.readLine(); // PERSONAGEM
-                algumaLinha = bufferedReader.readLine();
-                algumCapitulo.setPersonagem(personagens.get(algumaLinha));
-                algumaLinha = bufferedReader.readLine(); // VIDA
-                algumaLinha = bufferedReader.readLine();
-                algumCapitulo.setVida(personagens.get(algumaLinha).getVida());
-                algumaLinha = bufferedReader.readLine(); // CONSEQUÊNCIA
-                algumaLinha = bufferedReader.readLine();
-                if (!algumaLinha.equalsIgnoreCase("null")) {
-                    String vidaConsequencia = algumaLinha;
-                    vidaConsequencia = bufferedReader.readLine();
-                    algumCapitulo.setConsequencia(personagens.get(algumaLinha)
-                            .getMensagemAtk((personagens.get(algumaLinha)), Integer.parseInt(vidaConsequencia)));
-                    algumaLinha = bufferedReader.readLine();
-                } else {
-                    algumCapitulo.setConsequencia(null);
-                    algumaLinha = bufferedReader.readLine();
-                    algumaLinha = bufferedReader.readLine();
-                }
-                algumaLinha = bufferedReader.readLine(); // FINAL DO CAPÍTULO
-                if (!algumaLinha.equalsIgnoreCase("null")) {
-                    algumCapitulo.setFinalCap(algumaLinha);
-                } else {
-                    algumCapitulo.setFinalCap(null);
-                }
-                capitulos.put(algumCapitulo.getNome(), algumCapitulo);
-                bufferedReader.close();
             }
+            scanner.close();
         } catch (Exception e) {
             System.out.println("Erro ao carregar algum capítulo.");
         }
